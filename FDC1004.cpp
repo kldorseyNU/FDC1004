974b8bcb6680b913cdecc2d15b07606523d24387
kldorsey <109479372+kldorseyNU@users.noreply.github.com>
2024-07-23 14:44:10 -0400

    add adjustable rate

--- FDC1004.cpp
+++ FDC1004.cpp
@@ -1,131 +1,142 @@
-/***********************************************************************
- FDC1004 Library
-
- This library provides functions for using TI's FDC1004 Capacitance to Digital Sensor
+/**************************************************************
+ * Example Program for FDC1004 Library
+ * This program will demonstrates reading the capacitance on CAP1 of the FDC1004
+ * and changing capdac to try to auto-range the chip
+ **************************************************************
+ * Setup
+ * Connect 3.3V and Ground to the FDC1004 (don't use 5V, you'll fry your chip!)
+ * Connect SDA and SCL to your Arduino
+ * Power on and run this code
+ * Open a Serial monitor at 115200 baud
+ **************************************************************
+ * Read/write convenience functions written by Benjamin Shaya for Rest Devices
+ * bshaya@alum.mit.edu
+ * https://github.com/beshaya/FDC1004
+ **************************************************************
+ * Functions in this .ino file written by Kris Dorsey, Northeastern University
+ * k.dorsey@northeastern.edu
+ * https://github.com/kdorseyNU/FDC1004
+ **************************************************************/
  
- Written by Benjamin Shaya
-
-************************************************************************/
-
+#include <Wire.h>
 #include <FDC1004.h>
 
-uint8_t MEAS_CONFIG[] = {0x08, 0x09, 0x0A, 0x0B};
-uint8_t MEAS_MSB[] = {0x00, 0x02, 0x04, 0x06};
-uint8_t MEAS_LSB[] = {0x01, 0x03, 0x05, 0x07};
-uint8_t SAMPLE_DELAY[] = {11,11,6,3};
+FDC1004 fdc;
 
-FDC1004::FDC1004(uint16_t rate){
-  this->_addr = 0b1010000; //not configurable, to my knowledge
-  this->_rate = rate;
+////////////Constant measurement parameters/////////////
+const float capMax = 14.0;
+const float capdacConversion = 3.125;
+
+const uint8_t measSingl = 0; // 0 is single read, 1 is continuous read
+const uint8_t measCont = 1; // 0 is single read, 1 is continuous read
+
+const uint8_t measA = 0; 
+const uint8_t measB = 1; 
+const uint8_t measC = 2; 
+const uint8_t measD = 3; 
+
+const uint8_t pin1 = (uint8_t)FDC1004_Chan0;
+const uint8_t pin2 = (uint8_t)FDC1004_Chan1; //pin 2 must have a higher or same value as pin 1!
+const uint8_t pin3 = (uint8_t)FDC1004_Chan2; //pin 2 must have a higher or same value as pin 1!
+const uint8_t pin4 = (uint8_t)FDC1004_Chan3; //pin 2 must have a higher or same value as pin 1!
+
+///////////Parameters you can change//////////////
+uint8_t capdac = 0; // Will auto-adjust, but you can set to expected value for speed
+int readRate = 100; //does not currently work above 100 Hz
+
+/////////////////////////
+
+void setup() {
+  Serial.begin(115200);
+  delay(1000);
+  Wire.begin();
+  delay(1000);
+  fdc.resetDevice();
+  setCAPDAC(measA, pin2, pin2, measCont);
 }
 
-void FDC1004::write16(uint8_t reg, uint16_t data) {
-    //not sure I understand why this is written in 8 bits
-  Wire.beginTransmission(_addr);
-  Wire.write(reg); //send address
-  Wire.write( (uint8_t) (data >> 8));
-  Wire.write( (uint8_t) data);
-  Wire.endTransmission();
+void loop() {  
+  absoluteCapacitance(measA, pin1, pin1, measCont);
 }
 
-uint16_t FDC1004::read16(uint8_t reg) {
-  Wire.beginTransmission(_addr);
-  Wire.write(reg);
-  Wire.endTransmission();
-  uint16_t value;
-  //Wire.beginTransmission(_addr);
-  Wire.requestFrom(_addr, (uint8_t)2);
-  value = Wire.read();
-  value <<= 8;
-  value |= Wire.read();
-  //Wire.endTransmission();
-  return value;
+float absoluteCapacitance(uint8_t measSlot, uint8_t chanA, uint8_t chanB, uint8_t measType) {
+  float capacitanceRelative;
+  if (measType == 0){
+    capacitanceRelative =  configTrigRead(measSlot, chanA, chanB, measType);
+    }
+  else{
+    capacitanceRelative = relativeCapacitance(measSlot);
+    }
+  if ((capacitanceRelative <= capMax) && (capacitanceRelative >= -capMax)){
+    float capacitanceAbsolute = capacitanceRelative + (3.125 * (float)capdac); //converts capdac to pF
+    Serial.println(capacitanceAbsolute);
+    return capacitanceAbsolute;
+  }
+  else {
+    Serial.println("Capacitance out of bounds, re-running setCAPDAC");
+    setCAPDAC(measSlot, chanA, chanB, measType);
+    return -1;
+  }
 }
 
-//configure a measurement (call only when changing the setup of a measurement)
-uint8_t FDC1004::configureMeasurement(uint8_t measurement, uint8_t channel, uint8_t diffChannel, uint8_t capdac) {
-    //Verify data
-    if (!FDC1004_IS_MEAS(measurement) || !FDC1004_IS_CHANNEL(channel) || capdac > FDC1004_CAPDAC_MAX) {
-        Serial.println("Measurement or channel out of bounds");
-        return 1;
-    }
-
-    //build 16 bit measurement configuration
-    uint16_t configuration_data;
-    configuration_data = ((uint16_t)channel) << 13; //CHA diff
-
-    if (channel == diffChannel && capdac != 0){
-        //set CAPDAC, disable differential excitation on EXB
-        configuration_data |=  ((uint16_t)0x04) << 10; //CHB disable and CAPDAC enable
-        configuration_data |= ((uint16_t)capdac) << 5; //CAPDAC value
-    }
-    else if (channel == diffChannel && capdac == 0){
-        //Disable CAPDAC and differential excitation on EXB
-        configuration_data |=  ((uint16_t)0x05) << 10; //CHB disable and CAPDAC disable
-    }
-    else {
-        //disable CAPDAC, set differential excitation
-        configuration_data |=  ((uint16_t)diffChannel) << 10; //CHB enable and CAPDAC disable
-    }
-    write16(MEAS_CONFIG[measurement], configuration_data);
-    return 0;
+float relativeCapacitance(uint8_t measSlot) {  
+  delay(1000/readRate);
+  uint16_t value[2];
+  float capacitanceRelative = capMax + 1;
+  if (!fdc.readMeasurement(measSlot, value)) {
+    // The absolute capacitance is a function of the capdac and the read MSB (value[0])
+    // The measurement resolution is 0.5 fF and the max range is 30 pF 
+    // A 16 bit two's complement binary number ranges from -32768 to 32767 (i.e., resolution of ~0.46 fF with +/- 15 pF range)
+    // Therefore, we don't need to calculate the capacitance using the LSB since the most significant bit of LSB is below the measurement resolution
+    // We do still need to read LSB since the FDC1004 is expecting it to be read before the next measurement. The convenience functions in the .cpp file take care of this read.
+    int16_t capBits = value[0];
+    float capRatio = (15.0) / (32767.0); //This converts bits (2^15 - 1) into capacitance (+/- 15 pF range)
+    capacitanceRelative = (float)(capBits) * capRatio; // value in picofarads (pF)
+  }
+  return capacitanceRelative;
 }
 
-uint8_t FDC1004::triggerMeasurement(uint8_t measurement, uint8_t rate, uint8_t measType) {
-  //verify data
-    if (!FDC1004_IS_MEAS(measurement) || !FDC1004_IS_RATE(rate)) {
-       Serial.println("bad trigger request");
-       return 1;
-    }
+int setCAPDAC(uint8_t measSlot, uint8_t chanA, uint8_t chanB, uint8_t measType){
+  capdac = 0;
+  float capacitanceRelative = configTrigRead(measSlot, chanA, chanB, measType);
+  uint8_t capdacTimeout = 0;
 
-    uint16_t trigger_data;
-    trigger_data = ((uint16_t)rate) << 10; // sample rate
-    trigger_data |= measType << 8; 
-    trigger_data |= (1 << (7-measurement)); // 0 > bit 7, 1 > bit 6, etc
-    write16(FDC_REGISTER, trigger_data);
-    return 0;
-}
-
-uint8_t FDC1004::readMeasurement(uint8_t measurement, uint16_t * value) {
-    if (!FDC1004_IS_MEAS(measurement)) {
-        Serial.println("bad read request");
-        return 1;
-    }
-    
-    //check if measurement is complete
-    uint16_t fdcRegister = read16(FDC_REGISTER);
-    uint8_t delayCount = 0;
-    uint8_t maxDelayCount = 10;
-    while ((! (fdcRegister & ( 1 << (3-measurement)))) && delayCount < maxDelayCount) {
-      //confirm with FDC that measurement is complete
-      delay(10);
-      delayCount++;
-      fdcRegister = read16(FDC_REGISTER);
-    }
-
-    if (delayCount < maxDelayCount){
-      //read the value
-      uint16_t msb = read16(MEAS_MSB[measurement]);
-      uint16_t lsb = read16(MEAS_LSB[measurement]);  
-      value[0] = msb;
-      value[1] = lsb;
-      return 0;
+  while (capdacTimeout < 30){
+    if (adjustCAPDAC(capacitanceRelative)){
+      capdacTimeout = 31;
+      capacitanceRelative = configTrigRead(measSlot, chanA, chanB, measType);
+      return 1;
     }
     else{
-      //read failed or timed out
-      return 1;    
-    }  
+      capdacTimeout++;
+      capacitanceRelative = configTrigRead(measSlot, chanA, chanB, measType);
+    }
+  }
+  return 0;
 }
 
-//reset software of FDC1004
-uint16_t FDC1004::resetDevice() {
-  uint16_t resetData = 1 << 15; //reset is 0x1000
-  uint8_t delayCount = 0;
-  uint8_t maxDelayCount = 10;
-
-  write16(FDC_REGISTER, resetData);
-  delay(100);
-  uint16_t fdcRegister = read16(FDC_REGISTER);
-  return fdcRegister; //will return zero if properly reset
+int adjustCAPDAC(float capacitanceRelative) {  
+    if ((capacitanceRelative < capMax) && (capacitanceRelative > -1*capMax)){
+      // if it's in range, adjust capdac so capacitance is as close to 0 as possible
+      capdac += capacitanceRelative/capdacConversion;
+      capdac = max(capdac, ((uint8_t)0));
+      capdac = min(capdac, ((uint8_t)FDC1004_CAPDAC_MAX));
+      return 1;
+    }
+    else if (capacitanceRelative <= -1*capMax) {
+      capdac -= 5; //decrease by 15.625 pF, which is just outside the bounds of +/- 15 pF measurement range
+      capdac = max(capdac, ((uint8_t)0));
+      return 0;
+    }
+    else {
+      capdac += 5; //increase by 15.625 pF, which is just outside the bounds of +/- 15 pF measurement range
+      capdac = min(capdac, ((uint8_t)FDC1004_CAPDAC_MAX));
+      return 0;
+    } 
 }
 
+float configTrigRead(uint8_t measSlot, uint8_t chanA, uint8_t chanB, uint8_t measType){
+  fdc.configureMeasurement(measSlot, chanA, chanB, capdac);
+  fdc.triggerMeasurement(measSlot, FDC1004_100HZ, measType);
+  return relativeCapacitance(measSlot);
+}
